e1283ff384327a018b521a9890d1e53b96d5b2db
IaccinoMoura <jmouraiaccino@gmail.com>
2024-07-31 21:23:52 +0100

    praticando js

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,23 @@ Entre 25 e 29,99 Acima do peso
 */
 
 // peso / (altura*altura);
+
+var peso;
+var altura;
+var imc;
+var resultado;
+
+function calcular(event){
+    event.preventDefault();
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    imc = peso / (altura * altura);
+
+    if(imc < 17){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc + '<br/> Cuidado voce está muito abaixo do peso!'
+    }
+
+}
